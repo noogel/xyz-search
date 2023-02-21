1731a7922b30a71a4d8149f1bffa0bd852350a37
noogel <noogel@163.com>
2023-02-21 23:39:40 +0800

    opt:优化邮件请求和清理策略

--- src/main/java/noogel/xyz/search/infrastructure/filter/RequestFilter.java
+++ src/main/java/noogel/xyz/search/infrastructure/filter/RequestFilter.java
@@ -18,21 +18,20 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @Component
 public class RequestFilter implements Filter {
-    private static final Map<String, Long> IP_TIME_MAP = new ConcurrentHashMap<>();
+    private static final Map<String, Long> HASH_TIME_MAP = new ConcurrentHashMap<>();
     private static final long TIME_SHIFT = 600 * 1000L; // 10 分钟
 
-    @Scheduled(fixedRate = TIME_SHIFT)
+    @Scheduled(fixedRate = TIME_SHIFT * 144)
     public void removeExpiredRecord() {
-        log.info("remove expired request record.");
         // 移除过期 IP
         long nowTs = Instant.now().toEpochMilli();
-        ArrayList<String> ips = new ArrayList<>();
-        IP_TIME_MAP.forEach((key, val) -> {
+        ArrayList<String> expiredKey = new ArrayList<>();
+        HASH_TIME_MAP.forEach((key, val) -> {
             if (nowTs - val > TIME_SHIFT) {
-                ips.add(key);
+                expiredKey.add(key);
             }
         });
-        ips.forEach(IP_TIME_MAP::remove);
+        expiredKey.forEach(HASH_TIME_MAP::remove);
     }
 
     @Override
@@ -41,20 +40,22 @@ public class RequestFilter implements Filter {
         long nowTs = Instant.now().toEpochMilli();
         try {
             String remoteIP = IpUtils.getIpAddr(req);
+            String serverName = req.getServerName();
+            String hashKey = String.format("%s:%s", remoteIP, serverName);
             // 存在 key 并且 时差小于 x，则不发邮件。
-            if (IP_TIME_MAP.containsKey(remoteIP) && nowTs - IP_TIME_MAP.get(remoteIP) < TIME_SHIFT) {
+            if (HASH_TIME_MAP.containsKey(hashKey) && nowTs - HASH_TIME_MAP.get(hashKey) < TIME_SHIFT) {
                 // x 时间内不重复通知，不随访问更新。
                 // IP_TIME_MAP.put(remoteIP, nowTs);
                 log.info("畅文全索请求更新，ip:{}， 访问路径：{} {} 访问参数：{}", remoteIP, req.getMethod(),
                         req.getRequestURL(), req.getParameterMap());
             } else {
-                String subject = "畅文:" + remoteIP;
+                String subject = String.format("畅文: %s -> %s", remoteIP, serverName);
                 String msg = String.format("畅文全索请求通知：<br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
                                 "新 IP:%s，访问时间：%s，访问路径：%s %s，访问参数：%s",
                         remoteIP, LocalDateTime.now(), req.getMethod(), req.getRequestURL(), req.getParameterMap());
                 EmailNotifyHelper.send(subject, msg,
-                        ()-> !IP_TIME_MAP.containsKey(remoteIP),
-                        () -> IP_TIME_MAP.put(remoteIP, nowTs));
+                        ()-> !HASH_TIME_MAP.containsKey(hashKey),
+                        () -> HASH_TIME_MAP.put(hashKey, nowTs));
             }
         } catch (Exception ex) {
             log.error("畅文全索请求发送通知失败", ex);
