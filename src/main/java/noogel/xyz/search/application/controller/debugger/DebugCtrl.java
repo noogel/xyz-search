a42cf59b91585d4050297aadc26a1b712ee18d52
xyz-mini <xyz@xyz-minideMac-mini.local>
2025-03-02 22:48:48 +0800

    update

--- src/main/java/noogel/xyz/search/application/controller/debugger/DebugCtrl.java
+++ src/main/java/noogel/xyz/search/application/controller/debugger/DebugCtrl.java
@@ -13,35 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/debug")
 @Slf4j
 public class DebugCtrl {
-
-    // URL 格式验证
-    private static final String URL_REGEX = "^((http|https)://)?([\\w-]+\\.)+[\\w-]+(/[\\w-?=&/%.#]*)?$";
-    private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
-    private static final String ENG_REGEX = "^[A-Za-z',.!?]*$";
-    private static final Pattern ENG_PATTERN = Pattern.compile(ENG_REGEX);
-    private static final String DIG_REGEX = "^[0-9]*$";
-    private static final Pattern DIG_PATTERN = Pattern.compile(DIG_REGEX);
-    private static final String TWO_HAN_REGEX = "^[\\u4e00-\\u9fa5]{2}$";
-    private static final Pattern TWO_HAN_PATTERN = Pattern.compile(TWO_HAN_REGEX);
-    // 预编译正则提升性能
-    private static final Pattern CH_EN_PATTERN = Pattern.compile(
-            "([\\u4e00-\\u9fa5])([A-Za-z0-9])|([A-Za-z0-9])([\\u4e00-\\u9fa5])"
-    );
-
-    public static String insertSpaceBetweenChEn(String text) {
-        return CH_EN_PATTERN.matcher(text)
-                .replaceAll(mr ->
-                        mr.group(1) != null ?
-                                mr.group(1) + " " + mr.group(2) :
-                                mr.group(3) + " " + mr.group(4)
-                );
-    }
 
     @GetMapping("/test")
     public String debugTest() {
